0b9d57d6179a20970ae555e78e3f933d5aac3350
Johann Suarez <johann@gohash.tech>
2022-07-14 18:19:54 -0700

    Mailer sends mp3 files successfully

--- app/converter/ytdl.py
+++ app/converter/ytdl.py
@@ -2,10 +2,6 @@ from typing import List
 import subprocess
 from subprocess import PIPE
 
-
-#subprocess.run(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-o", "filename.mp3", "https://www.youtube.com/watch?v=r26VsCGCgb0"])
-#subprocess.run(["youtube-dl", "--extract-audio", "--restrict-filenames", "--audio-format", "mp3", "https://www.youtube.com/watch?v=r26VsCGCgb0"])
-
 class Converter:
     """
     A wrapper around a command line program (in this case youtube-dl)
@@ -17,9 +13,16 @@ class Converter:
         self.flags_and_args: List[str] = [
             '--extract-audio',
             '--restrict-filenames',
+            '-o ~/%(title)s.%(etx)s',
             '--audio-format',
             'mp3'
         ]
+
+    @staticmethod
+    def follow_up() -> None:
+        """
+        Manages the downloaded file, calling Mailer on it, and cleaning up afterwards.
+        """
 
     def convert(self, url: str) -> subprocess.Popen:
         """
@@ -33,5 +36,3 @@ class Converter:
         # Return the process, so we can check stdout/stderr
         proc = subprocess.Popen(self.command, stdout=PIPE, stderr=PIPE)
         return proc
-
-
--- app/mailer/mailer.py
+++ app/mailer/mailer.py
@@ -12,32 +12,28 @@ class Mailer:
         self.email_address: str = os.environ.get('EMAIL_ADDRESS') or 'nil'
         self.password: str = os.environ.get('EMAIL_PASS') or 'nil'
 
-    def compose_email(self, item: str, recipient: str):
+    def compose_email(self, recipient: str, item: str):
         """
         Build the e-mail containing the converted mp3 data.
         """
         EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS') or 'nil'
         EMAIL_PASSWORD = os.environ.get('EMAIL_PASS') or 'nil'
 
-        #contacts = ['YourAddress@gmail.com', 'test@example.com']
-
         msg = EmailMessage()
         msg['Subject'] = 'Your Converted Video'
         msg['From'] = EMAIL_ADDRESS
         msg['To'] = recipient
 
-        #msg.set_content('This is a plain text email')
+        msg.set_content("The .mp3 file is attached to this e-mail.")
 
-        msg.add_alternative("""\
-        <!DOCTYPE html>
-        <html>
-            <body>
-                <h1 style="color:SlateGray;">MP3 Conversion</h1>
-            </body>
-        </html>
-        """, subtype='html')
-
+        with open(item, 'rb') as content_file:
+            content = content_file.read()
+            msg.add_attachment(content, maintype='audio', subtype='mpeg', filename=item)
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
             smtp.send_message(msg)
+
+
+x = Mailer()
+x.compose_email("johann.suarez92@gmail.com", "song.mp3")
