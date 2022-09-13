4fe1cdf67dd03c66ae4109805aa3f28825d3c01c
Johann Suarez <johann@gohash.tech>
2022-09-13 00:26:29 -0700

    Added a static page as a front end

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app import Configs
 from .routers import yt2mp3
 
@@ -14,8 +15,11 @@ app.add_middleware(
 )
 
 app.include_router(yt2mp3.router)
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
 
+'''
 @app.get("/")
 async def root():
     return {"message": "Health check, yeah we're up!"}
 
+'''
