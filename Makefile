70957611788abb4fcd9958e65d1e8b887ec5a52e
Johann Suarez <johann.suarez92@gmail.com>
2022-09-29 10:51:23 -0700

    Moved static files and HTML

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
-	uvicorn app.main:app --host 0.0.0.0 --port 8000
+	uvicorn app.main:app --host 0.0.0.0 --port 9000
 
 clean:
 	rm *.mp3
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 from app import Configs
 from .routers import yt2mp3
+
 
 app = FastAPI()
 
@@ -15,11 +18,18 @@ app.add_middleware(
 )
 
 app.include_router(yt2mp3.router)
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
 
-'''
+app.mount(
+        "/static", 
+        StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"), 
+        name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-async def root():
-    return {"message": "Health check, yeah we're up!"}
-
-'''
+async def root(request: Request):
+    return templates.TemplateResponse(
+            "index.html",
+            {"request": request}
+    )
